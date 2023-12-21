a4281d9a9c2cd3e864cb8f1aa8671fb0a30e336c
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 22:31:40 +0100

    Update get_reddit_clean_field.sql

--- macros/snippets/utils/get_reddit_clean_field.sql
+++ macros/snippets/utils/get_reddit_clean_field.sql
@@ -3,7 +3,7 @@
     {%- if column_name == 'id' -%}
         {{column_name}}::varchar as {{table_name|trim('s')}}_{{column_name}}
 
-    {%- elif column_name in ("name","configured_status","effective_status","is_processing","account_id","ad_group_id","campaign_id","business_id") -%}
+    {%- elif column_name in ("name","configured_status","effective_status","is_processing") -%}
         {{column_name}} as {{table_name|trim('s')}}_{{column_name}}
 
     {%- else -%}
