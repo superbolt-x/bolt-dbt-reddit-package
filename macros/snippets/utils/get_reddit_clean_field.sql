b728e2174807dd3677100bed96b74dfc529a7bb1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 17:00:56 +0100

    Create get_reddit_clean_field.sql

---
+++ macros/snippets/utils/get_reddit_clean_field.sql
@@ -0,0 +1,14 @@
+{%- macro get_reddit_clean_field(table_name, column_name) %}
+
+    {%- if column_name == 'id' -%}
+        {{column_name}}::varchar as {{table_name|trim('s')}}_{{column_name}}
+
+        {%- elif column_name in ("name","configured_status","effective_status","is_processing","account_id","ad_group_id","campaign_id") -%}
+        {{column_name}} as {{table_name|trim('s')}}_{{column_name}}
+
+        {%- else -%}
+        {{column_name}}
+    {%- endif -%}
+        
+
+{% endmacro -%}
