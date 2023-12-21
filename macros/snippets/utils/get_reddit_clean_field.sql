867b246e52cf082a0afa330517fde2efee0ba411
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 21:34:26 +0100

    Update get_reddit_clean_field.sql

--- macros/snippets/utils/get_reddit_clean_field.sql
+++ macros/snippets/utils/get_reddit_clean_field.sql
@@ -1,9 +1,16 @@
 {%- macro get_reddit_clean_field(table_name, column_name) %}
+    {%- set reject_fields = [
+    "id",
+    "business_id",
+    "name",
+    "currency",
+    "_fivetran_synced"
+    ] -%}
 
     {%- if column_name == 'id' -%}
         {{column_name}}::varchar as {{table_name|trim('s')}}_{{column_name}}
 
-    {%- elif column_name in ("name","configured_status","effective_status","is_processing","account_id","ad_group_id","campaign_id") -%}
+    {%- elif column_name in ("name","configured_status","effective_status","is_processing","account_id","ad_group_id","campaign_id","business_id") -%}
         {{column_name}} as {{table_name|trim('s')}}_{{column_name}}
 
     {%- else -%}
