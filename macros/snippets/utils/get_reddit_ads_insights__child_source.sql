fdc100cf6461bf8939edabaffac16567a05edd46
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 17:45:39 +0100

    Update get_reddit_ads_insights__child_source.sql

--- macros/snippets/utils/get_reddit_ads_insights__child_source.sql
+++ macros/snippets/utils/get_reddit_ads_insights__child_source.sql
@@ -2,6 +2,7 @@
 
 
 {%- set action_types = dbt_utils.get_column_values(source('reddit_raw','ad_conversions_report'),'event_name') -%}
+{%- set reject_list = ['search','add_to_cart','view_content','page_visit','lead','add_to_wishlist','sign_up'] -%}
 
 SELECT 
     date,
@@ -9,13 +10,21 @@ SELECT
     {% for action_type in action_types -%}
     {%- set alias = conversion_alias_config(action_type) -%}
     {%- if alias|length %}
-        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}",
-        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{alias}}_value"
-        {%- if not loop.last %},{% endif %}
+        {%- if alias in reject_list %}
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}"
+            {%- if not loop.last %},{% endif %}
+        {%- else %}
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}",
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{alias}}_value"
+            {%- if not loop.last %},{% endif %}
     {%- else -%}
-        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}",
-        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{action_type}}_value"
-        {%- if not loop.last %},{% endif -%}
+        {%- if action_type in reject_list %}
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}"
+            {%- if not loop.last %},{% endif -%}
+        {%- else %}
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}",
+            COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{action_type}}_value"
+            {%- if not loop.last %},{% endif -%}
     {%- endif -%}
     {%- if not loop.last %},{%- endif %}
 
