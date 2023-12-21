2e3342f7616a3a38fc6033b8b1f8a05df9256aa2
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 22:37:00 +0100

    Update get_reddit_clean_field.sql

--- macros/snippets/utils/get_reddit_clean_field.sql
+++ macros/snippets/utils/get_reddit_clean_field.sql
@@ -1,13 +1,13 @@
 {%- macro get_reddit_clean_field(table_name, column_name) %}
 
     {%- if column_name == 'id' -%}
-        {{column_name}}::varchar as {{table_name|trim('s')}}_{{column_name}}
+        {{column_name}}::varchar as {{table_name|trim('s')}}_{{column_name}} 
 
     {%- elif column_name in ("name","configured_status","effective_status","is_processing") -%}
-        {{column_name}} as {{table_name|trim('s')}}_{{column_name}}
+        {{column_name}} as {{table_name|trim('s')}}_{{column_name}} 
 
     {%- else -%}
-        {{column_name}}
+        {{column_name}} 
     {%- endif -%}
         
 
