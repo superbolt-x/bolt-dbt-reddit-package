90124b56d271416994f86129d4d82597c4e0ce5e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 17:26:35 +0100

    Create reddit_ads.sql

---
+++ models/base/reddit_ads.sql
@@ -0,0 +1,24 @@
+{%- set selected_fields = [
+    "id",
+    "name",
+    "effective_status",
+    "account_id",
+    "ad_group_id",
+    "campaign_id",
+    "click_url"
+] -%}
+{%- set schema_name, table_name = 'reddit_raw', 'ad' -%}
+
+WITH staging AS 
+    (SELECT
+    
+        {% for field in selected_fields -%}
+        {{ get_reddit_clean_field(table_name, field) }},
+        {% endfor -%}
+
+    FROM {{ source(schema_name, table_name) }}
+    )
+
+SELECT *,
+    ad_id as unique_key
+FROM staging 
