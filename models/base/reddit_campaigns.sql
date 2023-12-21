a0f19eadd7f623cdba941c832b42f7d83b7524e9
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 17:35:02 +0100

    Create reddit_campaigns.sql

---
+++ models/base/reddit_campaigns.sql
@@ -0,0 +1,22 @@
+{%- set selected_fields = [
+    "id",
+    "name",
+    "effective_status",
+    "account_id",
+    "objective"
+] -%}
+{%- set schema_name, table_name = 'reddit_raw', 'campaigns' -%}
+
+WITH staging AS 
+    (SELECT
+    
+        {% for field in selected_fields -%}
+        {{ get_reddit_clean_field(table_name, field) }},
+        {% endfor -%}
+
+    FROM {{ source(schema_name, table_name) }}
+    )
+
+SELECT *,
+    campaign_id as unique_key
+FROM staging 
