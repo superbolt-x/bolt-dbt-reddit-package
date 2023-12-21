b51b7f71f54be3be2b459ac09da75c08a45a0647
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 16:50:51 +0100

    Create get_reddit_ads_insights__child_source.sql

---
+++ macros/snippets/utils/get_reddit_ads_insights__child_source.sql
@@ -0,0 +1,26 @@
+{%- macro get_reddit_ads_insights__child_source(table_name) -%}
+
+
+{%- set action_types = dbt_utils.get_column_values(source('reddit_raw','ad_conversions'),'event_name') -%}
+
+SELECT 
+    date,
+    ad_id,
+    {% for action_type in action_types -%}
+    {%- set alias = conversion_alias_config(action_type) -%}
+    {%- if alias|length %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as {{alias}}
+        {%- if not loop.last %},{% endif %}
+    {%- else -%}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN value ELSE 0 END), 0) as "{{action_type}}"
+        {%- if not loop.last %},{% endif -%}
+    {%- endif -%}
+    {%- if not loop.last %},{%- endif %}
+
+{% endfor %}
+
+    FROM {{ source('reddit_raw','ad_conversions') }}
+
+    GROUP BY 1,2
+
+{%- endmacro %}
