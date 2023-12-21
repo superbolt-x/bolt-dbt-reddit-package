624a8b529735ec739f8869d03d87347d8c7e0541
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 20:37:48 +0100

    Update reddit_performance_by_ad.sql

--- models/reporting/reddit_performance_by_ad.sql
+++ models/reporting/reddit_performance_by_ad.sql
@@ -5,7 +5,7 @@
 
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
 {%- set exclude_fields = ['date','day','week','month','quarter','year','unique_key'] -%}
-{%- set dimensions = ['account_id','campaign_id','adset_id','ad_id'] -%}
+{%- set dimensions = ['account_id','ad_id'] -%}
 {%- set measures = adapter.get_columns_in_relation(ref('reddit_ads_insights'))
                     |map(attribute="name")
                     |reject("in",exclude_fields)
