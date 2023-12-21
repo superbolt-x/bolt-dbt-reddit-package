24a14828f26810d252de315959558a83b3c5d8dc
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 21:38:59 +0100

    Update get_reddit_ads_insights__child_source.sql

--- macros/snippets/utils/get_reddit_ads_insights__child_source.sql
+++ macros/snippets/utils/get_reddit_ads_insights__child_source.sql
@@ -2,7 +2,6 @@
 
 
 {%- set action_types = dbt_utils.get_column_values(source('reddit_raw','ad_conversions_report'),'event_name') -%}
-{%- set reject_list = ['search','add_to_cart','view_content','page_visit','lead','add_to_wishlist','sign_up'] -%}
 
 SELECT 
     date,
@@ -10,14 +9,14 @@ SELECT
     {% for action_type in action_types -%}
     {%- set alias = conversion_alias_config(action_type) -%}
     {%- if alias|length %}
-        {%- if alias in reject_list %}
+        {%- if alias in ("search","add_to_cart","view_content","page_visit","lead","add_to_wishlist","sign_up") %}
             COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}"
         {%- else %}
             COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}",
             COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{alias}}_value"
         {%- endif -%}
     {%- else -%}
-        {%- if action_type in reject_list %}
+        {%- if action_type in ("search","add_to_cart","view_content","page_visit","lead","add_to_wishlist","sign_up") %}
             COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}"
         {%- else %}
             COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}",
