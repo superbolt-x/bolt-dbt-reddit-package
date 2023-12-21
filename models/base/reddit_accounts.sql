1f21eb9d86b5ebabcd27d7c5b42998b0dc455c18
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-21 17:23:46 +0100

    Create reddit_accounts.sql

---
+++ models/base/reddit_accounts.sql
@@ -0,0 +1,24 @@
+{%- set selected_fields = [
+    "id",
+    "business_id",
+    "name",
+    "currency",
+    "_fivetran_synced"
+] -%}
+{%- set schema_name, table_name = 'reddit_raw', 'accounts' -%}
+
+WITH staging AS 
+    (SELECT
+    
+        {% for field in selected_fields -%}
+        {{ get_reddit_clean_field(table_name, field) }},
+        {% endfor -%}
+        MAX(_fivetran_synced) OVER (PARTITION BY id) as last_updated_time
+
+    FROM {{ source(schema_name, table_name) }}
+    )
+
+SELECT *,
+    account_id as unique_key
+FROM staging 
+WHERE _fivetran_synced = last_updated_time
