4a2949c2a670a7efdb4205169b98324178c3c61e
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-27 09:42:20 +0100

    Create get_reddit_campaigns_insights__child_source.sql

---
+++ macros/snippets/utils/get_reddit_campaigns_insights__child_source.sql
@@ -0,0 +1,33 @@
+{%- macro get_reddit_campaigns_insights__child_source(table_name) -%}
+
+
+{%- set action_types = dbt_utils.get_column_values(source('reddit_raw','campaign_conversions_report'),'event_name') -%}
+
+SELECT 
+    date,
+    campaign_id,
+    {% for action_type in action_types -%}
+    {%- set alias = conversion_alias_config(action_type) -%}
+    {%- if alias|length %}
+        {%- if alias in ("search","add_to_cart","view_content","page_visit","lead","add_to_wishlist","sign_up") %}
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}"
+        {%- else %}
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{alias}}",
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{alias}}_value"
+        {%- endif -%}
+    {%- else -%}
+        {%- if action_type in ("search","add_to_cart","view_content","page_visit","lead","add_to_wishlist","sign_up") %}
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}"
+        {%- else %}
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_items ELSE 0 END), 0) as "{{action_type}}",
+            COALESCE(SUM(CASE WHEN event_name = '{{action_type}}' THEN total_value ELSE 0 END), 0) as "{{action_type}}_value"
+        {%- endif -%}
+    {%- endif -%}
+    {%- if not loop.last %},{% endif -%}
+    {% endfor %}
+
+    FROM {{ source('reddit_raw','campaign_conversions_report') }}
+
+    GROUP BY 1,2
+
+{%- endmacro %}
